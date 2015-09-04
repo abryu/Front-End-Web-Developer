19d52485e4be0237eae653fd81bdc258c6c9e5ec
abryu <wenboyu2208@gmail.com>
2015-09-04 17:06:39 -0400

    Update

--- frondend-nanodegree-neighborhood-map-master/js/app.js
+++ frondend-nanodegree-neighborhood-map-master/js/app.js
@@ -1,26 +1,32 @@
+var markersList = [];
 var initialLocations = [
     {
       name : "University of Toronto",
       description : "My dream school!",
-      position : {lat: 43.662892, lng: -79.395656}
+      position : {lat: 43.662892, lng: -79.395656},
+      index : 0
     },
     {
       name : "Ryerson University",
       description : "Good school!",
-      position : {lat: 43.657658, lng: -79.378802}
+      position : {lat: 43.657658, lng: -79.378802},
+      index : 1
     },
     {
       name : "Toronto City Hall",
       description : "Have been there two times!",
-      position : {lat: 43.653483, lng: -79.384094}
+      position : {lat: 43.653483, lng: -79.384094},
+      index : 2
     },
     {
       name : "Toronto Symphony Orchestra",
       description : "I like Classical Music!!",
-      position : {lat: 43.646287, lng: -79.386327}
+      position : {lat: 43.646287, lng: -79.386327},
+      index : 3
     },
 ];
 
+/*
 function initMap() {
   var infowindow = new google.maps.InfoWindow();
 
@@ -44,4 +50,59 @@ function initMap() {
     });
   };
 };
+*/
+
+var ViewModel = function() {
+  var self = this;
+
+  self.userSelected = ko.observable("");
+
+  self.updatedUserSelectedName = ko.computed(function() {
+        return self.userSelected().name;
+  });
+
+  self.updatedUserSelectedIndex = ko.computed(function() {
+        return self.userSelected().index;
+  });
+
+  self.updatedUserSelectedPosition = ko.computed(function() {
+        return self.userSelected().position;
+  });
+
+  function initMap() {
+    var infowindow = new google.maps.InfoWindow();
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: {lat: 43.653226, lng: -79.3831843}
+    });
+
+    for(var i = 0; i < initialLocations.length; i++) {
+      var marker = new google.maps.Marker({
+        name : initialLocations[i].name,
+        position: initialLocations[i].position,
+        map: map,
+        title: initialLocations[i].description
+      }); 
+
+      google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(this.name + "<br>" + this.title);
+          infowindow.open(map, this);
+          map.panTo(this.position);
+      });
+
+      markersList.push(marker);
+    };
+
+    self.testUpdate = ko.computed(function() {
+        google.maps.event.trigger(markersList[self.updatedUserSelectedIndex()], 'click');
+    });
+  };
+
+  google.maps.event.addDomListener(window, "load", initMap);
+
+
+};
+
+ko.applyBindings(new ViewModel());
 
