8e550f7d863050ef7945a8c87347e2f9c9092938
abryu <wenboyu2208@gmail.com>
2015-09-05 20:01:26 -0400

    After the success of AJAX (one-dim array)

--- frondend-nanodegree-neighborhood-map-master/js/app.js
+++ frondend-nanodegree-neighborhood-map-master/js/app.js
@@ -1,23 +1,27 @@
 var markersList = [];
+var infoWindowList = [];
+var infoWindowElement;
+
 var initialLocations = [
+    {
+      name : "Ryerson University",
+      description : "Good school!",
+      position : {lat: 43.657658, lng: -79.378802},
+      index : 0
+    },
     {
       name : "Toronto City Hall",
       description : "A symbol of Toronto!",
       position : {lat: 43.653483, lng: -79.384094},
-      index : 0
+      index : 1
     },
     {
       name : "University of Toronto",
       description : "My dream school!",
       position : {lat: 43.662892, lng: -79.395656},
-      index : 1
-    },
-    {
-      name : "Ryerson University",
-      description : "Good school!",
-      position : {lat: 43.657658, lng: -79.378802},
       index : 2
     },
+
     {
       name : "Toronto Symphony Orchestra",
       description : "I like Classical Music!!",
@@ -25,32 +29,6 @@ var initialLocations = [
       index : 3
     },
 ];
-
-/*
-function initMap() {
-  var infowindow = new google.maps.InfoWindow();
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: {lat: 43.653226, lng: -79.3831843}
-  });
-
-  for(var i = 0; i < initialLocations.length; i++) {
-    var marker = new google.maps.Marker({
-      name : initialLocations[i].name,
-      position: initialLocations[i].position,
-      map: map,
-      title: initialLocations[i].description
-    }); 
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(this.name + "<br>" + this.title);
-        infowindow.open(map, this);
-        map.panTo(this.position);
-    });
-  };
-};
-*/
 
 var ViewModel = function() {
   var self = this;
@@ -69,31 +47,40 @@ var ViewModel = function() {
         return self.userSelected().position;
   });
 
-  var infoWindowElement;
-  var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + 'toronto city hall' + '&format=json&callback=wikiCallback';
+  for(var j = 0; j < initialLocations.length; j++) {
 
-  var wikiRequestTimeout = setTimeout(function() {
-    infoWindowElement = "We are sorry for we cannot get wikipedia sources";
-  },8000);
+      var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + initialLocations[j].name + '&format=json&callback=wikiCallback';
 
-  $.ajax({
-    url: wikiUrl,
-    dataType: "jsonp",
-    success: function(response) {
-      var articleList = response[1];
-      var articleStr = "";
-      for (var i = 0; i < 1; i++) {
-        articleStr = articleList[i];
-        var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-        infoWindowElement = '<li class="list-group-item"><a href="' + url + '" target="_blank">' + articleStr + '</a></li>';
-      };
-      clearTimeout(wikiRequestTimeout);
-    }
-  });
+      console.log(wikiUrl);
 
+      var wikiRequestTimeout = setTimeout(function() {
+        infoWindowElement = "We are sorry for we cannot get wikipedia sources";
+      },8000);
+
+      $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        success: function(response) {
+          var articleList = response[1];
+          var articleStr = "";
+          for(var i = 0; i < 1; i++) {
+            articleStr = articleList[i];
+            var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+            infoWindowElement = '<li><a href="' + url + '" target="_blank">' + articleStr + '</a></li>';
+            console.log(articleStr + ' for ajax');
+          };
+          clearTimeout(wikiRequestTimeout);
+          console.log(infoWindowElement);
+          infoWindowList.push(infoWindowElement);
+        }
+      });    
+      infoWindowList.sort();
+  };
+  
   function initMap() {
-
     var infowindow = new google.maps.InfoWindow();
+
+    console.log(infoWindowList.length);
 
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 15,
@@ -101,15 +88,19 @@ var ViewModel = function() {
     });
 
     for(var i = 0; i < initialLocations.length; i++) {
+
+      console.log(infoWindowList[i]);
+
       var marker = new google.maps.Marker({
         name : initialLocations[i].name,
         position: initialLocations[i].position,
         map: map,
-        title: initialLocations[i].description
+        title: initialLocations[i].description,
+        info: infoWindowList[i]
       }); 
 
       google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(this.name + "<br>" + this.title + "<br>  <a href='https://www.google.ca/'>google</a>" + infoWindowElement);
+          infowindow.setContent(this.name + "<br>" + this.title + "<br>" + this.info);
           infowindow.open(map, this);
           map.panTo(this.position);
       });
@@ -123,7 +114,6 @@ var ViewModel = function() {
   };
 
   google.maps.event.addDomListener(window, "load", initMap);
-
 
 };
 
